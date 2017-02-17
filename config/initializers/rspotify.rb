2511edd8e758dcc5000fa718bfdd24c63c930748
superkevin@gmail.com <superkevin@gmail.com>
2017-02-16 20:04:53 -0500

    pull spotify creds into secrets.yml to prepare to launch this

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,1 +1,1 @@
-RSpotify::authenticate("27f69eac4316489db5f3cabd08e4a3ee", "9d672b18818245609aa37e14144b5edb")
+RSpotify::authenticate(Rails.application.secrets.spotify_client_id, Rails.application.secrets.spotify_client_secret)
