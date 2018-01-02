d15ad76f971f61a1992de4bb65bf8a8652338d46
Kevin Finn <kevin@relevant.healthcare>
2018-01-02 16:06:24 -0500

    cleanup and a bug fix

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -38,11 +38,16 @@ class Playlist < ApplicationRecord
   end
 
   def fresher_than_syncs?
+    return false if spotify_syncs.recently_completed.empty?
+    return true if FeedUpdate.recently_completed.empty?
+
     spotify_syncs.recently_completed.first.completed_at > FeedUpdate.recently_completed.first.completed_at
   end
 
   def fresher_than_opinions?
-    spotify_syncs.recently_completed.first.completed_at >
-      user.song_opinions.maximum(:updated_at)
+    return false if spotify_syncs.recently_completed.empty?
+    return true if user.song_opinions.empty?
+
+    spotify_syncs.recently_completed.first.completed_at > user.song_opinions.maximum(:updated_at)
   end
 end
